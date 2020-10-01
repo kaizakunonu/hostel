68f52695ff5fb9197355cf92d137bbea78cd8b5b
Kaiza Ilomo <kaiza@kunonu.tech>
2020-10-01 16:13:11 +0300

    Designed skeleton views for People sub-menus

--- models/models.py
+++ models/models.py
@@ -3,9 +3,10 @@
 from odoo import models, fields, api
 
 
+# definition of Hostel class start
 class Hostel(models.Model):
     _name = 'hostel.hostel'
-    _description = 'hostel.hostel'
+    _description = 'Hostel information'
 
     name = fields.Char(string='Hostel name', required=True, index=True,
                        help='Write the official hostel name')
@@ -25,44 +26,107 @@ class Hostel(models.Model):
             record.safe_capacity = float(record.capacity) * 0.8
 
 
+# HostelNurse class definition start
 class HostelNurse(models.Model):
     _name = 'hostel_nurse.hostel_nurse'
     _description = 'Hostel nurses and/or doctors'
 
-    name = fields.Char(string='Nurse name', required=True, index=True)
-    mobile_no = fields.Char(string='Mobile number', required=True, index=True)
-    start_date = fields.Date(string='Contract start date', required=True, index=True)
+    name = fields.Char(string='Hostel\'s nurse full name', required=True, index=True,
+                       help='Write the full name of hostel nurse in charge')
+    mobile_no = fields.Char(string='Mobile number', required=True, index=True,
+                            help='Write the mobile number of the nurse in charge')
+    start_date = fields.Date(string='Contract start date', required=True, index=True,
+                             help='Write the employment contract start date')
+    end_date = fields.Date(string='Contract end date', required=True, index=True,
+                           help='Write the employment contract end date')
 
 
+######################################
+# start of HostelCleaner class #######
+######################################
 class HostelCleaner(models.Model):
     _name = 'hostel_cleaner.hostel_cleaner'
     _description = 'Hostel Cleaners'
 
-    name = fields.Char(string='Cleaner name', required=True, index=True)
-    mobile_no = fields.Char(string='Mobile number', required=True, index=True)
-    start_date = fields.Date(string='Contract start date', required=True, index=True)
+    name = fields.Char(string='Hostel\'s cleaner full name', required=True, index=True,
+                       help='Write the full name of hostel cleaner in charge')
+    mobile_no = fields.Char(string='Mobile number', required=True, index=True,
+                            help='Mobile number of the hostel cleaner')
+    start_date = fields.Date(string='Contract start date', required=True, index=True,
+                             help='Write the employment contract start date')
+    end_date = fields.Date(string='Contract end date', required=True, index=True,
+                           help='Write the employment contract end date')
 
 
+#####################################
+# start of HostelResident class #####
+#####################################
 class HostelResident(models.Model):
     _name = 'hostel_resident.hostel_resident'
-    _description = 'hostel_resident.hostel_resident'
+    _description = 'Hostel resident information'
 
-    name = fields.Char(string='Resident name', required=True, index=True)
-    registration_no = fields.Char(string='Registration number', required=True, index=True)
-    room_no = fields.Integer(string='Room number', required=True, index=True)
-    rent_fee = fields.Float(string='Rent fee', digits=(6, 2), required=True, index=True)
+    name = fields.Char(string='Hostel resident name', required=True, index=True,
+                       help='Write the full name of hostel resident')
+    registration_no = fields.Char(string='Resident registration number', required=True, index=True,
+                                  help='Write the university registration number of the resident')
+    room_no = fields.Integer(string='Resident room number', required=True, index=True,
+                             help='Write the number of the room assigned to this resident')
+    rent_fee = fields.Float(string='Rent fee', digits=(6, 2), required=True, index=True,
+                            help='Enter amount of money paid by the resident as room rent fee')
 
 
+#########################################
+# start of HostelSecurityGuard class ####
+#########################################
+class HostelSecurityGuard(models.Model):
+    _name = 'hostel_security_guard.hostel_security_guard'
+    _description = 'Hostel security guard information'
+
+    name = fields.Char(string='Security guard full name', required=True, index=True,
+                       help='Write the full name of hostel security guard')
+    mobile_no = fields.Char(string='Mobile number', required=True, index=True,
+                            help='Mobile number of the hostel security guard')
+    start_date = fields.Date(string='Contract start date', required=True, index=True,
+                             help='Write the employment contract start date')
+    end_date = fields.Date(string='Contract end date', required=True, index=True,
+                           help='Write the employment contract end date')
+
+
+##########################################
+# start of HostelWarden class ############
+##########################################
+class HostelWarden(models.Model):
+    _name = 'hostel_warden.hostel_warden'
+    _description = 'Hostel warden information'
+
+    name = fields.Char(string='Warden full name', required=True, index=True,
+                       help='Write the full name of hostel warden')
+    mobile_no = fields.Char(string='Mobile number', required=True, index=True,
+                            help='Mobile number of the hostel warden')
+    start_date = fields.Date(string='Contract start date', required=True, index=True,
+                             help='Write the employment contract start date')
+    end_date = fields.Date(string='Contract end date', required=True, index=True,
+                           help='Write the employment contract end date')
+
+
+###############################################
+# start of HostelRoom class ###################
+###############################################
 class HostelRoom(models.Model):
     _name = 'hostel_room.hostel_room'
-    _description = 'hostel_room.hostel_room'
+    _description = 'Hostel room information'
 
-    name = fields.Char(string='Room name', required=True, index=True)
-    room_no = fields.Integer(string='Room number', required=True, index=True)
+    name = fields.Integer(string='Room number', required=True, index=True,
+                          help='Write the number of the hostel room')
+    first_resident = fields.Char(string='Name of first resident', required=True, index=True,
+                                 help='Write the name the first resident of the hostel room')
     rent_fee = fields.Float(string='Rent fee', digits=(6, 2), required=True, index=True)
     rented = fields.Boolean(string='Is it occupied?', required=True, default=False, index=True)
 
 
+###################################################
+# start of HostelAccommodation class ##############
+###################################################
 class HostelAccommodation(models.Model):
     _name = 'hostel_service.hostel_service'
     _description = 'hostel_service.hostel_service'
